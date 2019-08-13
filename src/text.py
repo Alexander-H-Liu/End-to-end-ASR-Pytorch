ba64aa5d4d3b3d023087f94a27564193f607ca77
Tzu-Wei Sung <windqaq@gmail.com>
2019-08-13 23:27:16 +0800

    add {Character,Subword,Word}TextEncoder

---
+++ src/text.py
@@ -0,0 +1,140 @@
+"""Modified from tensorflow_datasets.features.text.*
+
+Reference: https://www.tensorflow.org/datasets/api_docs/python/tfds/features/text_lib
+"""
+import abc
+
+
+class _BaseTextEncoder(abc.ABC):
+    @abc.abstractmethod
+    def encode(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def decode(self):
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def vocab_size(self):
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def load_from_file(cls, vocab_file):
+        raise NotImplementedError
+
+    @property
+    def pad_idx(self):
+        return 0
+
+    @property
+    def eos_idx(self):
+        return 1
+
+    @property
+    def unk_idx(self):
+        return 2
+
+    def __repr__(self):
+        return "<{} vocab_size={}>".format(type(self).__name__, self.vocab_size)
+
+
+class CharacterTextEncoder(_BaseTextEncoder):
+    def __init__(self, vocab_list):
+        # Note that vocab_list must not contain <pad>, <eos> and <unk>
+        # <pad>=0, <eos>=1, <unk>=2
+        self._vocab_list = ["<pad>", "<eos>", "<unk>"] + vocab_list
+        self._vocab2idx = {v: idx for idx, v in enumerate(self._vocab_list)}
+
+    def encode(self, s):
+        # Always strip trailing space, \r and \n
+        s = s.strip("\r\n ")
+        # Manually append eos to the end
+        return [self.vocab_to_idx(v) for v in s] + [self.eos_idx]
+
+    def decode(self, ids):
+        vocabs = []
+        for i in ids:
+            v = self.idx_to_vocab(i)
+            if v in ["<pad>", "<eos>"]:
+                break
+            vocabs.append(v)
+        return "".join(vocabs)
+
+    @classmethod
+    def load_from_file(cls, vocab_file):
+        with open(vocab_file, "r") as f:
+            # Do not strip space because character based text encoder should
+            # have a space token
+            vocab_list = [line.strip("\r\n") for line in f]
+        return cls(vocab_list)
+
+    @property
+    def vocab_size(self):
+        return len(self._vocab_list)
+
+    def vocab_to_idx(self, vocab):
+        return self._vocab2idx.get(vocab, self.unk_idx)
+
+    def idx_to_vocab(self, idx):
+        return self._vocab_list[idx]
+
+
+class SubwordTextEncoder(_BaseTextEncoder):
+    def __init__(self, spm):
+        if spm.pad_id() != 0 or spm.eos_id() != 1 or spm.unk_id() != 2:
+            raise ValueError(
+                "Please train sentencepiece model with following argument:\n"
+                "--pad_id=0 --eos_id=1 --unk_id=2 --bos_id=-1 --model_type=bpe --eos_piece=<eos>")
+        self.spm = spm
+
+    def encode(self, s):
+        return self.spm.encode_as_ids(s)
+
+    def decode(self, ids):
+        for i, x in enumerate(ids):
+            if x == self.eos_idx:
+                break
+        return self.spm.decode_ids(ids[:i])
+
+    @classmethod
+    def load_from_file(cls, filepath):
+        import sentencepiece as splib
+
+        spm = splib.SentencePieceProcessor()
+        spm.load(filepath)
+        spm.set_encode_extra_options(":eos")
+        return cls(spm)
+
+    @property
+    def vocab_size(self):
+        return len(self.spm)
+
+
+class WordTextEncoder(CharacterTextEncoder):
+    def encode(self, s):
+        # Always strip trailing space, \r and \n
+        s = s.strip("\r\n ")
+        # Space as the delimiter between words
+        words = s.split(" ")
+        # Manually append eos to the end
+        return [self.vocab_to_idx(v) for v in words] + [self.eos_idx]
+
+    def decode(self, ids):
+        vocabs = []
+        for i in ids:
+            v = self.idx_to_vocab(i)
+            if v in ["<pad>", "<eos>"]:
+                break
+            vocabs.append(v)
+        return " ".join(vocabs)
+
+
+def load_text_encoder(mode, vocab_file):
+    if mode == "character":
+        return CharacterTextEncoder.load_from_file(vocab_file)
+    elif mode == "subword":
+        return SubwordTextEncoder.load_from_file(vocab_file)
+    elif mode == "word":
+        return WordTextEncoder.load_from_file(vocab_file)
+    else:
+        raise NotImplementedError("`{}` is not yet supported.".format(mode))
